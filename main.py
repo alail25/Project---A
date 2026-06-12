32aa16bacb20e24e6ed0944b370f8a34a564332d
alail25 <alail25.b013@gmail.com>
2026-06-12 22:03:33 +0900

    Refactor main.py to implement Discord bot commands
    Removed the meme dictionary and input functionality, and added Discord bot commands to fetch and display a random fox image.

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            "ROFL": "tanggapan terhadap lelucon",
-            "SHEESH": "sedikit ketidaksetujuan",
-            "CREEPY": "menakutkan, tidak menyenangkan",
-            "AGGRO": "untuk menjadi agresif/marah",
-            }
-            for i in range (5)
-            word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+import discord
+from discord.ext import commands
+import requests
 
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("Kata nggak ada di kamus")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+def get_fox_image_url():
+    url = "https://randomfox.ca/floof/"
+    data = requests.get(url).json()
+    return data["image"]
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def fox(ctx):
+    await ctx.send(get_fox_image_url())
+
+    embed = discord.Embed(
+        title="🦆 Random Duck",
+        description="Berikut bebek acak untukmu!"
+    )
+    embed.set_image(url=image_url)
+
+    await ctx.send(embed=embed)
+
+bot.run("Y")
